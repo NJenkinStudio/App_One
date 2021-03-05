2ce45b95b980be1dabcf9010dc245ea61cb3649f
Koshokuar <koshokuar@rocketmail.com>
2021-03-05 13:15:14 -0500

    add testing for categories

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,34 @@
+class CategoriesController < ApplicationController
+  # before_action :set_category, only: [:show]
+
+  def new 
+    @category = Category.new
+  end 
+
+  def create 
+    @category = Category.new(category_params)
+    if @category.save 
+      flash[:notice] = "Category succesfully created"
+      redirect_to @category 
+    else
+      render 'new'
+    end
+  end
+
+  def index  
+
+  end
+
+  def show 
+    @category = Category.find(params[:id])
+  end
+
+  private
+  # def set_category 
+  #   @category = Category.find(category_params[:id])
+  # end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
